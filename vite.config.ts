eb36c9b8000233ef36cd2840d9f2ed1f46e2ea3c
hus-sayn <134363247+hus-sayn@users.noreply.github.com>
2026-09-21 16:24:39 +0500

    Update Vite Configurable

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // listen on both IPv4 and IPv6
+    port: 5173,
+  },
   resolve: {
     alias: {
       '@': path.resolve(import.meta.dirname, './src'),
